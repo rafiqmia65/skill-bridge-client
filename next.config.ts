962989810a50a825195931820b243ef3ca81c72e
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-02-19 17:44:28 +0600

    Fix: Cookie not being set during deployment issue resolved

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
+import { env } from "./env";
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*", // catch prefix/all path
-        destination: "https://skill-bridge-server-two.vercel.app/api/:path*",
+        source: "/api/auth/:path*",
+        destination: `${env.NEXT_PUBLIC_BACKEND_API_URL}/api/auth/:path*`,
       },
     ];
   },
